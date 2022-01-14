0eb3e6bdfa48b7fe5af1ada11bc6e65597255b8b
Rakib Ali <r.ali@se18.qmul.ac.uk>
2022-01-14 15:00:59 +0000

    NPM modules
    Learn about global imports and local imports and the libraries available in NPM

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,12 @@
+const validator = require("validator");
+const chalk = require("chalk").default;
 const getNotes = require("./notes");
 
 console.log(getNotes());
+
+console.log(validator.isEmail("Hello@gogle.com"));
+
+console.log(validator.isURL("www.hello.com"));
+
+console.log(chalk.green("Success!"));
+console.log(chalk.blue.bold.inverse("Error"));
