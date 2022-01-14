3355f26163a0d68d6364ad76168bf1bd85428edf
Rakib Ali <r.ali@se18.qmul.ac.uk>
2022-01-14 19:16:19 +0000

    yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,23 +1,12 @@
 const validator = require("validator");
 const chalk = require("chalk").default;
+const yargs = require("yargs");
 const getNotes = require("./notes");
 
-console.log(getNotes());
-
-console.log(validator.isEmail("Hello@gogle.com"));
-
-console.log(validator.isURL("www.hello.com"));
-
-console.log(chalk.green("Success!"));
-
-const input = process.argv[2];
-
-if (input) {
-  if (input.toLowerCase() === "test") {
-    console.log(
-      chalk.green.inverse("Congratulations, Your test was succesful")
-    );
-  } else if (input.toLowerCase() === "bad") {
-    console.log(chalk.red.inverse("Error your input is Invalid!"));
-  }
-}
+yargs.command({
+  command: "add",
+  describe: "add a note to your notebook",
+  handler: function () {
+    console.log("Added note");
+  },
+});
