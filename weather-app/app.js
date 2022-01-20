b91c42519193cc9876f54a41a5f096cdbe020076
rakib2067 <rakib7@hotmail.co.uk>
2022-01-20 00:48:38 +0000

    HTTP requests and error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,26 +2,36 @@ const request = require("request");
 const weatherKey = "eb256eba88cb08519851d4e6cb09de1b";
 
 const url =
-  "http://api.weatherstack.com/current?access_key=eb256eba88cb08519851d4e6cb09de1b&query=London";
+  "http://api.weatherstack.com/current?access_key=eb256eba88cb08519851d4e6cb09de1b&query=Hackney";
 
 request({ url: url, json: true }, (error, response) => {
-  data = response.body.current;
-  console.log(
-    `${data.weather_descriptions[0]}: It is currently ${data.temperature} degrees outside. It feels like ${data.feelslike} degrees`
-  );
+  if (error) {
+    console.log("OS Level Error: Are you connected to Wifi?");
+  } else if (response.body.error) {
+    console.log(
+      `Error ${response.body.error.type}: ${response.body.error.info}`
+    );
+  } else {
+    data = response.body.current;
+    console.log(
+      `${data.weather_descriptions[0]}: It is currently ${data.temperature} degrees outside. It feels like ${data.feelslike} degrees`
+    );
+  }
 });
 
 // Geocoding
-
-url = `/geocoding/v5/mapbox.places/{search_text}.json`;
-let geoKey =
-  "pk.eyJ1IjoicmFraWI3NzciLCJhIjoiY2t5bHh2YmUwMHNkazJ3cG5uYjRlamN4NCJ9.lts4Fo4nACZEkn6vMP8Xiw";
 
 let newURL =
   "https://api.mapbox.com/geocoding/v5/mapbox.places/London.json?access_token=pk.eyJ1IjoicmFraWI3NzciLCJhIjoiY2t5bHh2YmUwMHNkazJ3cG5uYjRlamN4NCJ9.lts4Fo4nACZEkn6vMP8Xiw&limit=1";
 
 request({ url: newURL, json: true }, (error, response) => {
-  console.log(`You searched for: ${response.query[0]}/n
-  You received results for ${response.features}
-  `);
+  if (error) {
+    console.log("OS Level Error: Are you connected to Wifi?");
+  } else if (response.body.features.length === 0) {
+    console.log("Error: Invalid Location");
+  } else {
+    const data = response.body;
+    console.log(`You searched for: ${data.query[0]}\nLatitude: ${data.features[0].center[1]}\nLongitude: ${data.features[0].center[0]}
+    `);
+  }
 });
