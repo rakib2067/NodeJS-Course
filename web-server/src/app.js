6b62d7f1df76ced826f1603bc71183ef505bfe1c
rakib2067 <rakib7@hotmail.co.uk>
2022-01-21 21:02:19 +0000

    set up routes

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,6 +5,17 @@ const app = express();
 app.get("", (req, res) => {
   res.send("Hello Express");
 });
+app.get("/help", (req, res) => {
+  res.send("Help Page");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
+
+app.get("/weather", (req, res) => {
+  res.send("Weather Page");
+});
 
 app.listen(3000, () => {
   console.log("server is up on port 3000");
