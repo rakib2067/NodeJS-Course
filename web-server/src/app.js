8c9368173874c307bb5be8277f70700f05de6f17
rakib2067 <rakib7@hotmail.co.uk>
2022-01-21 21:37:51 +0000

    served html files

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,20 +1,18 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
 
+const publicDirectory = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectory));
+
 app.get("", (req, res) => {
   res.send("Hello Express");
 });
-app.get("/help", (req, res) => {
-  res.send("Help Page");
-});
-
-app.get("/about", (req, res) => {
-  res.send("About Page");
-});
 
 app.get("/weather", (req, res) => {
-  res.send("Weather Page");
+  res.send({ temp: "27 degrees", location: "London", precipitation: 23 });
 });
 
 app.listen(3000, () => {
