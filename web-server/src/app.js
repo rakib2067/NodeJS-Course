bb9aed81a232e737ddead0753d5fa4691a26644b
rakib2067 <rakib7@hotmail.co.uk>
2022-01-22 01:10:13 +0000

    Added partials through handlebars changed handlebars config

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,24 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
+// Define paths for express condig
 const publicDirectory = path.join(__dirname, "../public");
+const viewsDirectory = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
+// Set up handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsDirectory);
+hbs.registerPartials(partialsPath);
+
+// Set up static directory to serve
 app.use(express.static(publicDirectory));
 
-app.set("view engine", "hbs");
 app.get("", (req, res) => {
-  res.render("index", { title: "Weather App", name: "Rakib" });
+  res.render("index", { title: "Weather App", name: "Rakib Ali" });
 });
 
 app.get("/about", (req, res) => {
@@ -17,7 +26,11 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/help", (req, res) => {
-  res.render("help", { title: "Help", message: "How to Node?" });
+  res.render("help", {
+    title: "Help",
+    message: "How to Node?",
+    name: "Rakib Ali",
+  });
 });
 
 app.get("/weather", (req, res) => {
