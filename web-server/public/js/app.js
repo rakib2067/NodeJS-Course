095405a46c967f20207928cce3b89fa603d8f348
Rakib Ali <r.ali@se18.qmul.ac.uk>
2022-01-23 21:45:48 +0000

    adding form submission

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,12 +2,34 @@
 
 console.log("client side is loaded");
 
-fetch("http://localhost:3000/weather?address=London").then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(`${data.location}\n${data.forecast}`);
-    }
+// fetch("http://localhost:3000/weather?address=London").then((response) => {
+//   response.json().then((data) => {
+//     if (data.error) {
+//       console.log(data.error);
+//     } else {
+//       console.log(`${data.location}\n${data.forecast}`);
+//     }
+//   });
+// });
+
+const getForecast = (event) => {
+  const weatherForm = document.querySelector("#weatherForm");
+  const addressInput = document.querySelector("#address");
+
+  weatherForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const address = addressInput.value;
+    await fetch("http://localhost:3000/weather?address=" + address).then(
+      (response) => {
+        response.json().then((data) => {
+          if (data.error) {
+            console.log(data.error);
+          } else {
+            console.log(data.location);
+            console.log(data.forecast);
+          }
+        });
+      }
+    );
   });
-});
+};
