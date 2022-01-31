d77c13490d28b1d45e27457115977266bcadec57
rakib2067 <rakib7@hotmail.co.uk>
2022-01-31 23:52:46 +0000

    used the collections find method to fetch data from the db and use queries in conjunction

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -3,8 +3,6 @@ const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
 
 const id = new ObjectId();
-console.log(id);
-console.log(id.getTimestamp());
 
 MongoClient.connect(
   connectionURL,
@@ -14,43 +12,28 @@ MongoClient.connect(
       return console.log("Unable to connect to database");
     }
     const db = client.db(databaseName);
-    db.collection("users").insertOne(
-      { _id: id, name: "Imran Hussain", age: 23 },
-      (error, result) => {
-        if (error) {
-          return console.log("Unable to return user");
-        }
-        console.log(result);
+    db.collection("users").findOne({ name: "Imran Hussain" }, (error, user) => {
+      if (error) {
+        return "Unable to find query";
       }
-    );
+      console.log(user);
+    });
+    db.collection("tasks")
+      .find({ completed: false })
+      .toArray((error, data) => {
+        if (error) {
+          return error;
+        }
+        console.log(data);
+      });
 
-    // db.collection("users").insertMany(
-    //   [
-    //     { name: "Rakib Ali", age: 21 },
-    //     { name: "John Doe", age: 15 },
-    //     { name: "Sue Grey", age: 4 },
-    //     { name: "Peter Jones", age: 22 },
-    //   ],
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log("Unable to insert documents");
-    //     }
-    //     console.log(result);
-    //   }
-    // );
-
-    // db.collection("tasks").insertMany(
-    //   [
-    //     { description: "Finish NodeJS Course", completed: false },
-    //     { description: "Finish React Course", completed: true },
-    //     { description: "Get a Job", completed: false },
-    //   ],
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log("Unable to insert documents");
-    //     }
-    //     console.log(result);
-    //   }
-    // );
+    db.collection("users")
+      .find({ age: 21 })
+      .count((error, count) => {
+        if (error) {
+          return error;
+        }
+        console.log(count);
+      });
   }
 );
