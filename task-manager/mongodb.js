4ff9f5a1148f9331b31d65a08064e7d9b7707ab3
rakib2067 <rakib7@hotmail.co.uk>
2022-01-31 23:33:26 +0000

    fetch and add object id using object class

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,10 +1,10 @@
-// CRUD
-const mongodb = require("mongodb");
-
-const MongoClient = mongodb.MongoClient;
-
+const { MongoClient, ObjectId } = require("mongodb");
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
+
+const id = new ObjectId();
+console.log(id);
+console.log(id.getTimestamp());
 
 MongoClient.connect(
   connectionURL,
@@ -14,15 +14,15 @@ MongoClient.connect(
       return console.log("Unable to connect to database");
     }
     const db = client.db(databaseName);
-    // db.collection("users").insertOne(
-    //   { name: "Rakib Ali", age: 21 },
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log("Unable to return user");
-    //     }
-    //     console.log(result);
-    //   }
-    // );
+    db.collection("users").insertOne(
+      { _id: id, name: "Imran Hussain", age: 23 },
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to return user");
+        }
+        console.log(result);
+      }
+    );
 
     // db.collection("users").insertMany(
     //   [
