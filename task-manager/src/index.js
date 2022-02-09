8c53ba787ae588ee69cd23f0c5980b42425622a9
rakib2067 <rakib7@hotmail.co.uk>
2022-02-09 20:14:22 +0000

    Added Endpoints for getting all users and users by ID

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -3,6 +3,7 @@ require("./db/mongoose");
 //linking the mongoose file causes it to run and creates the connection to the database
 const User = require("./models/user");
 const Task = require("./models/task");
+const e = require("express");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -23,6 +24,29 @@ app.post("/users", (req, res) => {
     })
     .catch((error) => {
       res.status(400).send(error);
+    });
+});
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
     });
 });
 
