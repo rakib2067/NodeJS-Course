c92e176fa5aa3ef7cff12da26e64003a54d78e7b
rakib2067 <rakib7@hotmail.co.uk>
2022-03-20 19:10:55 +0000

    added patch endpoint

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -47,6 +47,33 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
+app.patch("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid Updates" });
+  }
+
+  try {
+    let response = await User.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!response) {
+      res.status(404).send();
+    }
+
+    res.send(response);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
 app.post("/tasks", async (req, res) => {
   try {
     const task = new Task(req.body);
