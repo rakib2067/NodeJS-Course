f361d8eec530725b373a0faa118f1f40cb7b697f
rakib2067 <rakib7@hotmail.co.uk>
2022-12-27 20:25:06 +0000

    middleware

--- vividly/server.js
+++ vividly/server.js
@@ -1,10 +1,11 @@
+const morgan = require('morgan');
 const express = require('express');
 const server = express();
 
 server.use(express.json());
-
+server.use(morgan('tiny'));
 //Base dir
-server.get('/', (req, res) => res.send('Vividly API - To Track all your favourite movies.'))
+server.get('/', (req, res) => res.send('Vividly API - To Track all your favourite movies.'));
 
 
 module.exports = server;
