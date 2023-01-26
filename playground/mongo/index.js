a884bd87084400a858b8c2f8e8dc52bfc6e0f8ee
rakib2067 <rakib7@hotmail.co.uk>
2023-01-26 23:16:37 +0000

    mongoose create

--- playground/mongo/index.js
+++ playground/mongo/index.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose
-  .connect("mongodb://localhost/playground")
+mongoose.connect('mongodb://127.0.0.1:27017/playground')
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
 
@@ -15,9 +14,22 @@ const courseSchema = new mongoose.Schema({
 
 const Course = mongoose.model("Course", courseSchema);
 
-const course = new Course({
-  name: "AWS - Certified Cloud Practicioner",
-  author: "Stephane Maarek",
-  tags: ["backend", "cloud"],
-  isPublished: true,
-});
+async function addCourse(){
+  const course = new Course({
+    name: "AWS - Certified Cloud Practicioner",
+    author: "Stephane Maarek",
+    tags: ["backend", "cloud"],
+    isPublished: true,
+  });
+
+  const result = await course.save();
+  console.log(result)
+}
+
+async function getCourses(){
+  const courses = await Course.find();
+  console.log(courses)
+}
+
+getCourses()
+
